97bef3e37982ad606f90bc1f00d72f304485363d
Martina Huzanic <143994576+martinaHuzanic@users.noreply.github.com>
2023-09-14 17:02:24 +0200

    Add files via upload

---
+++ TaskTwo/TaskTwo/src/main/java/Subject.java
@@ -0,0 +1,58 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Martina
+ */
+class Subject {
+    private final int id;
+    private String name;
+    private Teacher teacher;
+    private final List<Student> students = new ArrayList<>();
+
+    public Subject(int id, String name) {
+        this.id = id;
+        this.name = name;
+    
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+}
